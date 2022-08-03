3c3dcb643402eb00ec0b663c5b722e1d13274511
Sami Altuncu <saltuncu@bimser.com.tr>
2022-08-04 01:25:52 +0300

    Footer stil düzeltmesi

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import styles from '../styles/footer.module.scss';
 
 const Footer: React.FC<any> = () => {
     return (
-        <footer id="footer">
+        <footer className={styles.footer}>
+            <div className={styles.content}>
+                <span>repository</span>
+                <span>@ 2022 Sami Altuncu</span>
+            </div>
         </footer>
     )
 }
